1cabd059e2459ad98721d17817103a0e513cb29d
Krest_IV <krestsov.iv@yandex.ru>
2024-06-12 15:04:57 +0200

    Changes instigated by feedback renamed method added assertion for JUnit code example

--- src/test/java/tests/SearchJUnit5Test.java
+++ src/test/java/tests/SearchJUnit5Test.java
@@ -9,12 +9,22 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchJUnit5Test {
 
     @Test
-    void searchJUnit5(){
+    void searchJUnit5Test(){
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").shouldHave(text("wiki")).click();
         $("#wiki-tab").click();
         $$("[href$='SoftAssertions']").first().shouldHave(text("Soft assertions"));
         $$("[href$='SoftAssertions']").first().click();
         $$("h4.heading-element").findBy(text("JUnit5")).should(exist);
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class}) " +
+                "class Tests { " +
+                "  @Test " +
+                "  void test() { " +
+                "    Configuration.assertionMode = SOFT; " +
+                "    open(\"page.html\"); " +
+                "    $(\"#first\").should(visible).click(); " +
+                "    $(\"#second\").should(visible).click(); " +
+                "  } " +
+                "}"));
     }
 }
